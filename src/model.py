c7984006a6b5d388bb7a78f485c50577abef5873
mariam-hassan2 <121635911+mariam-hassan2@users.noreply.github.com>
2026-03-22 00:50:12 -0400

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+class TrafficCNN(nn.Module):
+    def __init__(self, num_classes=43):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 4 * 4, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
